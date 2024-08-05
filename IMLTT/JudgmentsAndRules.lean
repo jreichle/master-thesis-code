350e520e1033ca07acf120781fcabf1937d17d35
Johannes Reichle <reichlejohannes52@gmail.com>
2024-08-05 15:40:49 +0200

    Renaming and adjusting lifting

--- IMLTT/JudgmentsAndRules.lean
+++ IMLTT/JudgmentsAndRules.lean
@@ -44,9 +44,9 @@ mutual
     | pi_intro : HasType (Γ ⬝ A) b B
                  → HasType Γ (Tm.lam b) (Tm.pi A B)
     | sigma_intro : HasType Γ a A → HasType Γ b (substitute B a 0)
-                    → HasType Γ (Tm.pair a b) (Tm.sigma A B)
+                    → HasType Γ (Tm.pairSigma a b) (Tm.sigma A B)
     | iden_intro : IsType Γ A
-                   → HasType (Γ ⬝ A) (Tm.refl A 0) (Tm.iden A 0 0)
+                   → HasType (Γ ⬝ A) (Tm.refl 0) (Tm.iden A 0 0)
     -- universe intro
     | univ_unit : IsCtx Γ
                   → HasType Γ ⊤ U
@@ -60,18 +60,18 @@ mutual
                   → HasType ((Γ ⬝ A) ⬝ A) (Tm.iden A 0 1) U
     -- elimination rules (except univ)
     | unit_elim : IsType (Γ ⬝ Tm.unit) C → HasType Γ c (substitute C Tm.unit 0) → HasType Γ p Tm.unit
-                  → HasType Γ (Tm.ind_tt p c) (substitute C p 0)
+                  → HasType Γ (Tm.indUnit p c) (substitute C p 0)
     | empty_elim : IsType (Γ ⬝ ⊥) C → HasType Γ p ⊥
-                   → HasType Γ (Tm.ind_empty p) (substitute C p 0)
+                   → HasType Γ (Tm.indEmpty p) (substitute C p 0)
     | pi_elim : HasType Γ f (Tm.pi A B) → HasType Γ a A
                 → HasType Γ (Tm.app f a) (substitute B A 0)
     | sigma_elim₁ : HasType Γ z (Tm.sigma A B)
-                    → HasType Γ (Tm.prj₁ z) A
+                    → HasType Γ (Tm.prjSigma₁ z) A
     | sigma_elim₂ : HasType Γ z (Tm.sigma A B)
-                    → HasType Γ (Tm.prj₂ z) (substitute B (Tm.prj₁ z) 0)
+                    → HasType Γ (Tm.prjSigma₂ z) (substitute B (Tm.prjSigma₁ z) 0)
     | iden_elim : IsType (((Γ ⬝ A) ⬝ A) ⬝ (Tm.iden A a b)) C
                   → HasType (Γ ⬝ A) t
-                      (substitute (substitute (substitute C 0 2) 0 1) (Tm.refl A 0) 0)
+                      (substitute (substitute (substitute C 0 2) 0 1) (Tm.refl 0) 0)
                   → HasType (((Γ ⬝ A) ⬝ A) ⬝ (Tm.iden A 2 1)) (Tm.j t 2 1 0) C
 
     -- pi_Σ₁
@@ -83,19 +83,19 @@ mutual
   inductive IsEqualTerm : Ctx → Tm → Tm → Tm → Prop where
     -- computation rules
     | unit_comp : IsType (Γ ⬝ ⊤) C → HasType Γ c (substitute C Tm.tt 0)
-                  → IsEqualTerm Γ (Tm.ind_tt Tm.tt c) Tm.tt (substitute C Tm.tt 0)
+                  → IsEqualTerm Γ (Tm.indUnit Tm.tt c) Tm.tt (substitute C Tm.tt 0)
     | pi_comp : HasType (Γ ⬝ A) b B → HasType Γ a A
                 → IsEqualTerm Γ (Tm.app (Tm.lam b) a) (substitute b a 0) (substitute B a 0)
     | iden_comp : IsType (((Γ ⬝ A) ⬝ A) ⬝ (Tm.iden A a b)) C
                   → HasType (Γ ⬝ A) t (substitute (substitute (substitute C 0 2) 0 1)
-                      (Tm.refl A 0) 0)
-                  → IsEqualTerm (Γ ⬝ A) (Tm.j t 0 0 (Tm.refl A 0)) t
-                      (substitute (substitute (substitute C 0 2) 0 1) (Tm.refl A 0) 0)
+                      (Tm.refl 0) 0)
+                  → IsEqualTerm (Γ ⬝ A) (Tm.j t 0 0 (Tm.refl 0)) t
+                      (substitute (substitute (substitute C 0 2) 0 1) (Tm.refl 0) 0)
     | sigma_comp₁ : HasType Γ a A → HasType Γ b (substitute B a 0)
-                    → IsEqualTerm Γ (Tm.prj₁ (Tm.pair a b)) a A
+                    → IsEqualTerm Γ (Tm.prjSigma₁ (Tm.pairSigma a b)) a A
     | sigma_comp₂ : HasType Γ a A → HasType Γ b (substitute B a 0)
-                    → IsEqualTerm Γ (Tm.prj₂ (Tm.pair a b)) b
-                      (substitute B (Tm.prj₁ (Tm.pair a b)) 0)
+                    → IsEqualTerm Γ (Tm.prjSigma₂ (Tm.pairSigma a b)) b
+                      (substitute B (Tm.prjSigma₁ (Tm.pairSigma a b)) 0)
 end
 
 postfix : max " ctx" => IsCtx
--- IMLTT/Substitution.lean
+++ IMLTT/Substitution.lean
@@ -1,36 +1,44 @@
 import IMLTT.AbstractSyntax
 
--- TODO: use more efficient lifting (see e.g. van Benthem 1994 - Implementation LC or Dybier 2006 - NbE)
+-- TODO: use more efficient lifting
+-- - on LC lecture only apply lifting when really substituted
 
-/-- lift term to avoid capturing free variables of substituted term during substitution -/
-def lift (l : Nat) : Tm → Tm
+/- # Lifting -/
+-- applied to substituted term
+-- needed to preserver `original depth` of variables
+-- -> free variables need to refer to same context entry or constant
+-- currently according to Abel and colleagues (2007)
+def lift (l j : Nat) : Tm -> Tm
   | .unit => .unit
   | .empty => .empty
-  | .pi A B  => .pi (lift l A) (lift (l + 1) B)
-  | .sigma A B => .sigma (lift l A) (lift (l + 1) B)
-  | .iden A s t => .iden (lift l A) (lift l s) (lift l t)
+  | .pi A B  => .pi (lift l j A) (lift (l + 1) j B)
+  | .sigma A B => .sigma (lift l j A) (lift (l + 1) j B)
+  | .iden A s t => .iden (lift l j A) (lift l j s) (lift l j t)
   | .univ => .univ
   | .var i =>
       if i < l
         then .var i
-        else .var (i + 1)
+        else .var (i + j)
   | .tt => .tt
-  | .indUnit s t => .indUnit (lift l s) (lift l t)
-  | .indEmpty s => .indEmpty (lift l s)
-  | .lam t => .lam (lift (l + 1) t)
-  | .app s t => .app (lift l s) (lift l t)
-  | .pairSigma s t => .pairSigma (lift l s) (lift (l + 1) t)
-  | .prjSigma₁ s => .prjSigma₁ (lift l s)
-  | .prjSigma₂ s => .prjSigma₂ (lift l s)
-  | .refl t => .refl (lift l t)
-  | .j t a b p => .j (lift l t) (lift l a) (lift l b) (lift l p)
+  | .indUnit s t => .indUnit (lift l j s) (lift l j t)
+  | .indEmpty s => .indEmpty (lift l j s)
+  | .lam t => .lam (lift (l + 1) j t)
+  | .app s t => .app (lift l j s) (lift l j t)
+  | .pairSigma s t => .pairSigma (lift l j s) (lift (l + 1) j t)
+  | .prjSigma₁ s => .prjSigma₁ (lift l j s)
+  | .prjSigma₂ s => .prjSigma₂ (lift l j s)
+  | .refl t => .refl (lift l j t)
+  | .j t a b p => .j (lift l j t) (lift l j a) (lift l j b) (lift l j p)
 
+/- # Substitution -/
+-- lifting used
+-- decrease var that references binder outside the length of the substitution
 def substitute (s : Tm) (t : Tm) (j : Nat) : Tm :=
   match s with
   | .unit => .unit
   | .empty => .empty
-  | .pi A B  => .pi (substitute A t j) (substitute B (lift 0 t) (j + 1))
-  | .sigma A B => .sigma (substitute A t j) (substitute B (lift 0 t) (j + 1))
+  | .pi A B  => .pi (substitute A t j) (substitute B (lift 0 1 t) (j + 1))
+  | .sigma A B => .sigma (substitute A t j) (substitute B (lift 0 1 t) (j + 1))
   | .iden A m n => .iden A (substitute m t j) (substitute n t j)
   | .univ => .univ
   | .var i =>
@@ -42,14 +50,15 @@ def substitute (s : Tm) (t : Tm) (j : Nat) : Tm :=
   | .tt => .tt
   | .indUnit m n => .indUnit (substitute m t j) (substitute n t j)
   | .indEmpty m => .indEmpty (substitute m t j)
-  | .lam n => .lam (substitute n (lift 0 t) (j + 1))
+  | .lam n => .lam (substitute n (lift 0 1 t) (j + 1))
   | .app m n => .app (substitute m t j) (substitute n t j)
-  | .pairSigma m n => .pairSigma (substitute m t j) (substitute n (lift 0 t) (j + 1))
+  | .pairSigma m n => .pairSigma (substitute m t j) (substitute n (lift 0 1 t) (j + 1))
   | .prjSigma₁ m => .prjSigma₁ (substitute m t j)
   | .prjSigma₂ m => .prjSigma₂ (substitute m t j)
   | .refl m => .refl (substitute m t j)
   | .j u a b p => .j (substitute u t j) (substitute a t j) (substitute b t j) (substitute p t j)
 
+-- TODO: probably not needed?
 def substitute_ctx (Γ : Ctx) (t : Tm) (j : Nat) : Ctx :=
   match Γ with
   | .empty => .empty
