041192b9a1a8c7aa160076ab76beda3520ba4b8b
Johannes Reichle <reichlejohannes52@gmail.com>
2024-10-17 14:25:54 +0200

    Delete lifting proofs file

--- IMLTT/proofs/admissable/Lifting.lean
+++
@@ -1,6 +0,0 @@
-import IMLTT.AbstractSyntax
-import IMLTT.Substitution
-import IMLTT.JudgmentsAndRules
-
-theorem lifting_ctx_one_term : Γ ⬝ (lift i j A) = Γ, (lift_ctx i j (Ctx.empty ⬝ A)) :=
-  sorry
