cd5096a683c3c60842fc192373a0d22d82c0a29e
Johannes Reichle <reichlejohannes52@gmail.com>
2024-12-17 13:37:24 +0100

    Extract boudnary is_type_type_eq from hasType boundary

--- IMLTT/typed/proofs/admissable/Contexts.lean
+++ IMLTT/typed/proofs/admissable/Contexts.lean
@@ -10,45 +10,6 @@ import IMLTT.typed.proofs.admissable.Substitution
 
 import aesop
 
-theorem defeq_is_type : IsEqualType Γ A A' → IsType Γ A :=
-  by
-    intro hAA
-    apply IsEqualType.recOn
-      (motive_1 := fun Γ _hiC => IsCtx Γ)
-      (motive_2 := fun Γ A _hA => IsType Γ A)
-      (motive_3 := fun Γ a A _haA => HasType Γ a A)
-      (motive_4 := fun Γ A A' _hAA => IsType Γ A)
-      (motive_5 := fun Γ a a' A _haaA => HasType Γ a A) -- use boundary_has_type instead?
-      hAA
-    case unit_comp =>
-      intro n Γ A a hA haA _ _
-      apply HasType.unit_elim hA haA (HasType.unit_intro (boundary_ctx_term haA))
-    case sigma_comp =>
-      intro n Γ a A b B C c haA hbB hC hcC _ _ ihC _
-      apply HasType.sigma_elim
-      · apply HasType.sigma_intro haA hbB
-      · apply ihC
-      · apply hcC
-    case iden_comp =>
-      intro n Γ A B b a hB hbB _haA _ihB _ihbB ihaA
-      apply HasType.iden_elim
-      · apply hB
-      · apply hbB
-      · apply HasType.iden_intro 
-        apply ihaA
-    case iden_form_eq =>
-      intro n Γ A A' a₁ a₂ a₃ a₄ hAA haaA haaA' ihAA ihaaA ihaaA'
-      apply IsType.iden_form
-      · apply ihaaA
-      · apply HasType.ty_conv ihaaA' sorry -- FIXME: needs symmetry :(
-    -- any_goals
-    --   solve
-    --   | repeat'
-    --     first
-    --     | intro a
-    --     | aesop
-    any_goals sorry
-
 theorem defeq_is_term' : IsEqualTerm Γ a a' A → HasType Γ a' A :=
   by
     intro haaA
--- IMLTT/typed/proofs/boundary/BoundaryHasType.lean
+++ IMLTT/typed/proofs/boundary/BoundaryHasType.lean
@@ -6,32 +6,35 @@ import IMLTT.typed.JudgmentsAndRules
 import IMLTT.typed.proofs.boundary.BoundaryIsCtx
 import IMLTT.typed.proofs.admissable.DefeqSymm
 
-theorem boundary_has_type : IsEqualTerm Γ a a' A → HasType Γ a A :=
+theorem boundary_has_type :
+    (∀ {n : Nat} {Γ : Ctx n}, Γ ctx → Γ ctx) ∧
+    (∀ {n : Nat} {Γ : Ctx n} {A : Tm n}, (Γ ⊢ A type) → Γ ⊢ A type) ∧
+    (∀ {n : Nat} {Γ : Ctx n} {A a : Tm n}, (Γ ⊢ a ∶ A) → Γ ⊢ a ∶ A) ∧
+    (∀ {n : Nat} {Γ : Ctx n} {A A' : Tm n}, (Γ ⊢ A ≡ A' type) → Γ ⊢ A type) ∧
+    (∀ {n : Nat} {Γ : Ctx n} {A a a' : Tm n}, (Γ ⊢ a ≡ a' ∶ A) → Γ ⊢ a ∶ A) :=
   by
-    intro haaA
-    apply IsEqualTerm.recOn
+    apply judgment_recursor
       (motive_1 := fun Γ _hiC => IsCtx Γ)
       (motive_2 := fun Γ A _hA => IsType Γ A)
       (motive_3 := fun Γ a A _haA => HasType Γ a A)
-      (motive_4 := fun Γ A A' _hAA => IsType Γ A) -- FIXME: extract this?
+      (motive_4 := fun Γ A A' _hAA => IsType Γ A)
       (motive_5 := fun Γ a a' A _haaA => HasType Γ a A)
-      haaA
-    case unit_comp =>
+    case IsEqualTermUnitComp =>
       intro n Γ A a hA haA _ _
       apply HasType.unit_elim hA haA (HasType.unit_intro (boundary_ctx_term haA))
-    case sigma_comp =>
+    case IsEqualTermSigmaComp =>
       intro n Γ a A b B C c haA hbB _hC hcC _ _ ihC _
       apply HasType.sigma_elim
       · apply HasType.sigma_intro haA hbB
       · apply ihC
       · apply hcC
-    case iden_comp =>
+    case IsEqualTermIdenComp =>
       intro n Γ A B b a hB hbB _ _ _ ihaA
       apply HasType.iden_elim
       · apply hB
       · apply hbB
       · apply HasType.iden_intro ihaA
-    case iden_form_eq =>
+    case IsEqualTypeIdenFormEq =>
       intro n Γ A A' a₁ a₂ a₃ a₄
       intro hAA _haaA _haaA' _ihAA ihaaA ihaaA'
       apply IsType.iden_form
@@ -43,3 +46,15 @@ theorem boundary_has_type : IsEqualTerm Γ a a' A → HasType Γ a A :=
         first
         | intro a
         | aesop
+
+theorem boundary_is_type_type_eq : IsEqualType Γ A A' → IsType Γ A :=
+  by
+    intro hAA
+    apply (And.left (And.right (And.right (And.right boundary_has_type))))
+    apply hAA
+
+theorem boundary_has_type_term_eq : IsEqualTerm Γ a a' A → HasType Γ a A :=
+  by
+    intro haaA
+    apply (And.right (And.right (And.right (And.right boundary_has_type))))
+    apply haaA
